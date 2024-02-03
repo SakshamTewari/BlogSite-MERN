4fc735965aeb1358d0036c403e5077abc1fb27b2
Saksham <sakshamtewari3@gmail.com>
2024-02-04 02:33:38 +0530

    SignUp API route, hashing using bcryptjs

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,19 +1,22 @@
 const newError = require('http-errors');
 const User = require('../models/user.model');
+const bcrypt = require('bcryptjs');
 async function signup(req, res){
         const { username, email, password } = req.body;
 
         if(!username || !email || !password)
             return res.status(404).json({message:'404, fields required'});
         
+        const hashedPassword = bcrypt.hashSync(password,10);
+
         const newUser = new User({
             username,
             email,
-            password,
+            password: hashedPassword,
         });
-
+        console.log(newUser);
         try{
-            await newUser.save(); 
+            await newUser.save();
             res.json('SignUp successful');
         }
         catch(error){
